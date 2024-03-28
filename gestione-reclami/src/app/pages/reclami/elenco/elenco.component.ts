b35aa6f5e0aeac9b9e71526baf4f3f2ebf7c19bc
mouhssine <moussuper@gmail.com>
2024-03-28 15:52:39 +0100

    all in

--- gestione-reclami/src/app/pages/reclami/elenco/elenco.component.ts
+++ gestione-reclami/src/app/pages/reclami/elenco/elenco.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { complaint } from 'src/app/shared/models/complaint';
+import { ComplaintService } from 'src/app/shared/services/complaint.service';
+import { ComplaintTable } from 'src/app/shared/models/complaintTable';
 
 @Component({
   selector: 'app-elenco',
   templateUrl: './elenco.component.html',
   styleUrls: ['./elenco.component.scss']
 })
-export class ElencoComponent {
+export class ElencoComponent implements OnInit {
+  complaints: complaint[] = [];
+  elenco: ComplaintTable[] = [];
 
+  loadingIndicator: boolean = true;
+
+  constructor(private complaintService: ComplaintService) { }
+
+  ngOnInit(): void {
+    this.complaintService.getComplaints().subscribe(result => {
+      this.complaints = result;
+      this.elenco = this.complaints.map(c => ({
+        idReclamo: c.id,
+        dataSegnalazione: c.dataReclamo,
+        dataPresoInCarico: c.dettaglioReclamo.dataPresoInCarico,
+        dataChiusura: c.dettaglioReclamo.dataChiusura,
+        codiceNegozio: c.dettaglioReclamo.codiceNegozio,
+        manager: c.dettaglioReclamo.manager,
+        cliente: `${c?.customer?.nome} ${c?.customer?.cognome}`,
+        stato: c.dettaglioReclamo.stato,
+        gestione: c.dettaglioReclamo.gestione,
+        causaleReclamo: c.causale,
+        soddisfazione: c.dettaglioReclamo.soddisfazione
+      }));
+      console.log(this.elenco)
+
+      this.loadingIndicator = false;
+    });
+  }
 }
