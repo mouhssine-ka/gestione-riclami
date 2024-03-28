7d92d64ba29836fe81ddd643efa154a6a5548d46
mouhssine <moussuper@gmail.com>
2024-03-28 11:23:54 +0100

    Aggiornato model e mock

--- gestione-reclami/src/app/shared/models/complaint.ts
+++ gestione-reclami/src/app/shared/models/complaint.ts
@@ -1,3 +1,6 @@
+import { Customer } from "./customer";
+import { DettaglioReclamo } from "./elencoReclami";
+
 export interface complaint{
     id?: String;
     dataReclamo?: Date;
@@ -6,4 +9,6 @@ export interface complaint{
     regione?: String;
     provincia?: String;
     polo?: String;
+    customer?: Customer;
+    dettaglioReclamo: DettaglioReclamo;
 }
