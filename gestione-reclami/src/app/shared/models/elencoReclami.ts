f1d12f46b2b8b2764cda5faa1294f94c028a7446
mouhssine <moussuper@gmail.com>
2024-03-28 12:55:58 +0100

    .

---
+++ gestione-reclami/src/app/shared/models/elencoReclami.ts
@@ -0,0 +1,9 @@
+export interface DettaglioReclamo{
+    dataPresoInCarico?: Date;
+    dataChiusura?: Date;
+    codiceNegozio?: String;
+    manager?: String;
+    stato?: String;
+    gestione?: String;
+    soddisfazione?: boolean;
+}
